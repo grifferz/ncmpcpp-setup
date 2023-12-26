64154cee8d14c5c0f341ee595a9007c695de8928
Andy Smith <andy@arran.localnet>
2023-12-26 20:25:22 +0000

    initial import

---
+++ album_cover_poller.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+# $HOME/.ncmpcpp/album_cover_poller.sh
+
+# Andy Smith <andy@strugglers.net>
+# SPDX-License-Identifier: 0BSD
+
+ncmpcpp_home="$HOME/.ncmpcpp"
+default_cover="$ncmpcpp_home/default_cover.jpg"
+cover_dir="$XDG_RUNTIME_DIR/ncmpcpp"
+ncmpcpp_cover="$cover_dir/current_cover.jpg"
+
+green="$(tput setaf 2)"
+normal="$(tput sgr0)"
+
+display_centered_at_row() {
+    termwidth="$(tput cols)"
+    input="$1"
+    row="$2"
+
+    # This horrible sed that I got from somewhere strips out all ANSI escape
+    # sequences from the input, because otherwise we'll miscalculate how long
+    # it is. We still will get it wrong for multibyte Unicode stuff but meh…
+    cleaned_input=$(printf "%s" "$input" |
+        sed 's/\x1B[@A-Z\\\]^_]\|\x1B\[[0-9:;<=>?]*[-!"#$%&'"'"'()*+,.\/]*[][\\@A-Z^_`a-z{|}~]//g')
+    # Calculate length of string. Would "wc -m" do better at handling non-ASCII?
+    input_len="${#cleaned_input}"
+
+    if [ "$input_len" -ge "$termwidth" ]; then
+        start_pos=0
+    else
+        start_pos="$(( (termwidth - input_len) / 2))"
+    fi
+
+    printf '\e[%u;%uH' "$row" "$start_pos"
+    printf "%s\n" "$input"
+}
+
+show_cover() {
+    clear
+    # Get all the metadata from mpd.
+    mpc \
+        -f '%artist%\t%albumartist%\t%album%\t%date%\t%originaldate%' \
+        current | \
+    while IFS= read -r mpc_line; do
+        # This grossness needed to handle the case where a field might be
+        # empty. If we did the usual IFS=$'\t' then it would skip over the
+        # empty field, so we have to use mapfile to explode it into an array
+        # instead.
+        mapfile -td $'\t' mpc < <(printf %s "$mpc_line")
+        if [ -z "${mpc[1]}" ]; then
+            current_artist="${mpc[0]}"
+        else
+            current_artist="${mpc[1]}"
+        fi;
+
+        if [ -z "${mpc[4]}" ] && [ -z "${mpc[3]}" ]; then
+            # No date known at all.
+            current_year="unknown year"
+        elif [ -z "${mpc[4]}" ]; then
+            current_year="${mpc[3]}"
+        else
+            current_year="${mpc[4]}"
+        fi
+
+        if [ -z "${mpc[2]}" ]; then
+            current_album="Unknown Album"
+        else
+            current_album="${mpc[2]}"
+        fi
+
+        display_centered_at_row "${green}${current_artist}${normal}" 1
+        display_centered_at_row \
+            "'${green}${current_album}${normal}' (${green}${current_year}${normal})" 2
+        printf "\n"
+    done
+    timg --center "$ncmpcpp_cover"
+}
+
+if [ ! -d "$cover_dir" ]; then
+    mkdir -vp "$cover_dir"
+    cp "$default_cover" "$ncmpcpp_cover"
+fi
+
+if [ ! -f "$ncmpcpp_cover" ]; then
+    cp "$default_cover" "$ncmpcpp_cover"
+fi
+
+# Initially show whatever cover file is already there, which will either be
+# what we last played or the default cover.
+show_cover
+
+# And then wait on an inotify event indicating that something has closed our
+# cover image file after writing to it.
+while inotifywait \
+    --syslog --quiet --quiet --event close_write "$ncmpcpp_cover"; do
+    show_cover
+done
---
+++ track_change.sh
@@ -0,0 +1,205 @@
+#!/bin/bash
+
+# $HOME/.ncmpcpp/track_change.sh
+
+# Andy Smith <andy@strugglers.net>
+# SPDX-License-Identifier: 0BSD
+
+set -eu
+
+### Some things user might want to change.
+
+# This is the path to the music library as known by mpd.
+music_base="/path/to/your/music"
+
+# File name that each album's art is stored under.
+candidate_name="cover.jpg"
+
+# Directory to place the current track's cover in. Can be some temporary or
+# cache directory - we'll create it if it doesn't exist already.
+# This will typically be something like /run/user/1000/ncmpcpp
+cover_dir="$XDG_RUNTIME_DIR/ncmpcpp"
+
+# Where all your ncmpcpp files live, though we only use this as part of the
+# "default" cover image path.
+ncmpcpp_home="$HOME/.ncmpcpp"
+
+# Path to an image to use when we don't have a better album cover image.
+default_cover="$ncmpcpp_home/default_cover.jpg"
+
+# This path we'll copy the correct cover image to.
+ncmpcpp_cover="$cover_dir/current_cover.jpg"
+
+# How many milliseconds to keep the desktop notification visible for. A lot of
+# notiifcation daemons don't support this, e.g. GNOME just ignores it, but
+# Dunst and Wired do.
+notification_timeout=10000
+
+### No user-serviceable parts below here.
+
+error() {
+    logger -t "$(basename "$0")" "${*}"
+    printf "%s\\n" "%{*}" 1>&2
+}
+
+warning() {
+    logger -t "$(basename "$0")" "${*}"
+}
+
+use_album_cover() {
+    # ALways copy the default cover, because otherwqise the inotify in the
+    # poller won't trigger and album info for things with no cover will never
+    # be displayed.
+    # Otherwise only bother if the files are actually different (think -
+    # playing consecutive tracks of same album).
+    if [ "$1" = "$default_cover" ] || ! cmp --silent "$1" "$ncmpcpp_cover"; then
+        cp "$1" "$ncmpcpp_cover"
+    fi
+}
+
+do_desktop_notification() {
+    # ${meta[0]} = file
+    # ${meta[1]} = artist
+    # ${meta[2]} = title
+    # ${meta[3]} = albumartist
+    # ${meta[4]} = album
+    # ${meta[5]} = date
+    # ${meta[6]} = originaldate
+    local -n meta="$1"
+
+    local artist
+    local title
+    local albumartist
+    local year
+    local album
+
+    artist="${meta[1]}"
+    title="${meta[2]}"
+
+    # If we don't have an album artist then we'll just assume the track artist.
+    if [ -n "${meta[3]}" ]; then
+        albumartist="${meta[3]}"
+    else
+        albumartist="$artist"
+    fi
+
+    # If we know the original date, use that, else the date, else nothing.
+    if [ "${meta[6]+x}" ]; then
+        year="${meta[6]}"
+    elif [ "${meta[5]+x}" ]; then
+        year="${meta[5]}"
+    fi
+
+    if [ -z "${meta[4]+x}" ]; then
+        album="Unknown Album"
+    else
+        album="${meta[4]}"
+    fi
+
+    # Add the year on the end of the album if we have it.
+    if [ -n "$year" ]; then
+        album="$album • $year"
+    fi
+
+    # libnotify as supported by most desktops. e.g. in Debian it's from
+    # libnotify-bin package. Your typical GNOME desktop notification.
+    #
+    # Set normal priority as otherwise GNOME hides it away in the notification
+    # tray.
+    #
+    # Set transient so it doesn't stack them up inside the tray.
+    #
+    # Transient hint not necessary on GNOME but might be elsewhere.
+    #
+    # image-path hint uses the album cover as the notification icon. This seems
+    # quite unreliable on GNOME, like it caches an old one for ages. Other
+    # notification daemons are more helpful.
+    notify-send \
+        --app-name MPD \
+        --urgency normal \
+        --expire-time="$notification_timeout" \
+        --transient \
+        --hint="int:transient:1" \
+        --hint="string:image-path:$ncmpcpp_cover" \
+        "$artist • $title" \
+        "$albumartist • $album"
+}
+
+# Get all the metadata from mpd.
+# I sure hope none of it ever contains a tab…
+while IFS= read -r mpc_line; do
+    # This grossness needed to handle the case where a field might be
+    # empty. If we did the usual IFS=$'\t' then it would skip over the
+    # empty field, so we have to use mapfile to explode it into an array
+    # instead.
+    mapfile -td $'\t' mpc < <(printf %s "$mpc_line")
+done < <(mpc \
+    -f '%file%\t%artist%\t%title%\t%albumartist%\t%album%\t%date%\t%originaldate%' \
+    current)
+
+track_file="${mpc[0]}"
+full_track_file="$music_base/$track_file"
+
+# Sanity checks - can't work out an album cover without knowing an existing
+# directory path.
+if [ ! -r "$full_track_file" ]; then
+    error "mpd said that file '$full_track_file' is currently playing but it
+doesn't seem to exist"
+    use_album_cover "$default_cover"
+else
+    if [ ! -d "$cover_dir" ]; then
+        mkdir -p "$cover_dir"
+    fi
+
+    # The $track_file can be in these various formats:
+    #
+    # a) Artist/Album/Track.mp3
+    # b) Various Artists/Album/Track.mp3
+    # c) Artist/Track.mp3
+    #
+    # Case (c) is not handled yet as this is for album art and we don't know
+    # the album. In future we might try to extract cover art from the file, or
+    # look for it in a separate file once we know where to look on a per-file
+    # basis.
+
+    # This removes everything that's NOT a "/" out of the track file path.
+    only_slashes="${track_file//[^\/]}"
+    # So we can count them.
+    slash_count="${#only_slashes}"
+
+    case "$slash_count" in
+        "2")
+            containing_dir="$(dirname "$full_track_file")"
+
+            if [ -r "$containing_dir/$candidate_name" ]; then
+                use_album_cover "$containing_dir/$candidate_name"
+            else
+                use_album_cover "$default_cover"
+            fi
+            ;;
+        "1")
+            warning "non-album track '$track_file' not handled yet"
+            use_album_cover "$default_cover"
+            ;;
+        *)
+            warning "Don't know how to handle track path '$full_track_file'"
+            use_album_cover "$default_cover"
+            ;;
+    esac
+fi
+
+# ${mpc[0]} = file
+# ${mpc[1]} = artist
+# ${mpc[2]} = title
+# ${mpc[3]} = albumartist
+# ${mpc[4]} = album
+# ${mpc[5]} = date
+# ${mpc[6]} = originaldate
+#
+# We need at least an artist and a title to do a desktop notification.
+if [ -z "${mpc[1]}" ] || [ -z "${mpc[2]}" ]; then
+    warning "Current track '$full_track_file' didn't give us an artist and title,
+so no desktop notification"
+else
+    do_desktop_notification mpc
+fi
